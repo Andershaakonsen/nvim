fd2315248915d5abae85d865142d2afee9ce332c
Andershaakonsen <64076839+Andershaakonsen@users.noreply.github.com>
2023-02-18 14:37:35 +0100

    sample coc-config

--- lua/josean/plugins-setup.lua
+++ lua/josean/plugins-setup.lua
@@ -11,6 +11,7 @@ local ensure_packer = function()
 end
 local packer_bootstrap = ensure_packer() -- true if packer was just installed
 
+
 -- autocommand that reloads neovim and installs/updates/removes plugins
 -- when file is saved
 vim.cmd([[ 
@@ -124,29 +125,12 @@ return packer.startup(function(use)
   -- git integration
   use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
-  -- lsp-zero test
-  use({
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v1.x",
-    requires = {
-      -- LSP Support
-      { "neovim/nvim-lspconfig" }, -- Required
-      { "williamboman/mason.nvim" }, -- Optional
-      { "williamboman/mason-lspconfig.nvim" }, -- Optional
+  use({ "neoclide/coc.nvim", branch = "release" })
+  use("lervag/vimtex")
 
-      -- Autocompletion
-      { "hrsh7th/nvim-cmp" }, -- Required
-      { "hrsh7th/cmp-nvim-lsp" }, -- Required
-      { "hrsh7th/cmp-buffer" }, -- Optional
-      { "hrsh7th/cmp-path" }, -- Optional
-      { "saadparwaiz1/cmp_luasnip" }, -- Optional
-      { "hrsh7th/cmp-nvim-lua" }, -- Optional
-
-      -- Snippets
-      { "L3MON4D3/LuaSnip" }, -- Required
-      { "rafamadriz/friendly-snippets" }, -- Optional
-    },
-  })
+  --[[ On new systems install COC packages via
+        CocInstall coc-vimtex coc-texlab
+    --]]
 
   if packer_bootstrap then
     require("packer").sync()
