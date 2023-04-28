d0e2060698e65345e95c14e49459e78ed885421c
Andershaakonsen <64076839+Andershaakonsen@users.noreply.github.com>
2023-04-28 20:46:07 +0200

    tresitter context

--- init.lua
+++ init.lua
@@ -18,6 +18,7 @@ require("anders.plugins.bufferline")
 require("anders.plugins.harpoon")
 require("anders.plugins.indent-blankline")
 require("anders.plugins.omnisharp")
+require("anders.plugins.treesitter-context")
 
 -- -- wndow transparancy
 vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
@@ -28,3 +29,4 @@ vim.api.nvim_set_hl(0, "NormalNC", { link = "Normal" })
 -- Set linenumber color
 vim.api.nvim_command("highlight CursorLineNr guifg=#ffffff")
 vim.api.nvim_command("highlight LineNr guifg=#a1a3a0")
+-- vim.api.nvim_command("highlight LineNr guifg=#4ee9e4")
